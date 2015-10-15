8f9e2edf41f6dc53830066bf4a97b534604013db
SchillerQuinn <SchillerQuinn@gmail.com>
2015-10-15 13:37:05 -0500

    added the one line version

--- Bookmarklet.js
+++ Bookmarklet.js
@@ -3,6 +3,9 @@
   script.src = 'https://raw.githubusercontent.com/SchillerQuinn/FormSubmitScript/master/Scraper.js';
   document.body.appendChild(script);
 })()
+
+<a href="javascript:(function(){var script=document.createElement('SCRIPT');script.src='https://raw.githubusercontent.com/SchillerQuinn/FormSubmitScript/master/Scraper.js';document.body.appendChild(script);})()"> Do Something! </a>
+
 /* TODO:
 Grab outputs,
 Show output.
