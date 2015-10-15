996c7d247ede307008f8c4ac72668d1cb1003292
SchillerQuinn <SchillerQuinn@gmail.com>
2015-10-15 13:59:12 -0500

    Tried a different thing

--- Bookmarklet.js
+++ Bookmarklet.js
@@ -1,10 +1,10 @@
-(function () {
-  var script = document.createElement('SCRIPT');
-  script.src = 'https://raw.githubusercontent.com/SchillerQuinn/FormSubmitScript/master/Scraper.js';
-  document.body.appendChild(script);
-})()
+javascript:(function(){
+  _my_script=document.createElement('SCRIPT');
+  _my_script.type='text/javascript';
+  _my_script.src='https://raw.githubusercontent.com/SchillerQuinn/FormSubmitScript/master/Scraper.js';
+  document.getElementsByTagName('head')[0].appendChild(_my_script);
+})();
 
-<a href="javascript:(function(){var script=document.createElement('SCRIPT');script.src='https://raw.githubusercontent.com/SchillerQuinn/FormSubmitScript/master/Scraper.js';document.body.appendChild(script);})()"> Do Something! </a>
 
 /* TODO:
 Grab outputs,
