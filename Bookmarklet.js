7ea29395924267d0df3e6eb55f97cd75995db432
SchillerQuinn <SchillerQuinn@gmail.com>
2015-10-15 14:08:57 -0500

    Update Bookmarklet.js

--- Bookmarklet.js
+++ Bookmarklet.js
@@ -1,6 +1,6 @@
 javascript:(function(){
   _my_script=document.createElement('SCRIPT');
-  _my_script.type='text/javascript';
+  _my_script.type='javascript';
   _my_script.src='https://raw.githubusercontent.com/SchillerQuinn/FormSubmitScript/master/Scraper.js';
   document.getElementsByTagName('head')[0].appendChild(_my_script);
 })();
