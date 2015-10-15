9e7ae5aa9aa06cff2976301ed69f77b7eef8a0e6
SchillerQuinn <SchillerQuinn@gmail.com>
2015-10-15 13:26:08 -0500

    Create Bookmarklet.js
    I am going to need to change the source once I have the scraper.

---
+++ Bookmarklet.js
@@ -0,0 +1,10 @@
+(function () {
+  var script = document.createElement('SCRIPT');
+  script.src = 'http://example.com/the_code.js';
+  document.body.appendChild(script);
+})()
+/* TODO:
+Change .src
+Grab outputs,
+Show output.
+*/
