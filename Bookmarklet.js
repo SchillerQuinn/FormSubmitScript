977bfbbf13e24ef249f0453a4829057cfdb1e1bc
SchillerQuinn <SchillerQuinn@gmail.com>
2015-10-20 13:10:44 -0500

    Switched to RawGit

--- Bookmarklet.js
+++ Bookmarklet.js
@@ -1,6 +1,6 @@
 javascript: (function () { 
     var jsCode = document.createElement('script'); 
-    jsCode.setAttribute('src', 'https://raw.githubusercontent.com/SchillerQuinn/FormSubmitScript/master/Scraper.js');                  
+    jsCode.setAttribute('src', 'https://cdn.rawgit.com/SchillerQuinn/FormSubmitScript/master/Scraper.js');                  
   document.body.appendChild(jsCode); 
  }());
 
