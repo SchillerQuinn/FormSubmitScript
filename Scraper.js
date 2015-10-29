02b0742cb344d13228550873d48235103548cb51
SchillerQuinn <SchillerQuinn@gmail.com>
2015-10-29 15:12:04 -0500

    Added in code to scrape page
    cant use jquery

--- Scraper.js
+++ Scraper.js
@@ -6,6 +6,7 @@
 		-will have to load in a popup or a side bar so they can copy and paste.
 	-automatically attaching to clipboard.
 	*/
+	/*
 	var v = "1.3.2";
 
 	if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
@@ -20,19 +21,19 @@
 		};
 		document.getElementsByTagName("head")[0].appendChild(script);
 	}
-	//testing to see if the website blocks non-console Jquery
-	$('body').hide()
-	console.log("well at least this worked")
-	/*
+	*/
+	
 	//Grab all of the different variables
 	### I don't know how to find paths or convert them into a form javascript can read ###
 	
-	var tNumber = scrapePage([path]);
-	var eventName = scapePage([path]);
-	var clientName = scrapePage([path]);
-	var techName = scrapePage([path]);
+	var tNumber = document.getElementById("clip_button_7_16_0_0_0_0_2_7_0_0_1_4_3_1_5_5_0_1_3_1_5_0_0_1_16_1_1_12_10").firstElementChild.text;
+	var eventName = document.getElementById("subject").value
+	var clientName = (document.getElementsByClassName("mailToLink")[0].firstElementChild).innerHTML.slice(14,-19) //this needs to be tested
+	var techName = document.getElementById("assignedTechPopup")[Number(document.getElementById("assignedTechPopup").value)+1].text
 	var startTime = scrapePage([path]); //make sure it is in Year-month-day+24hour time format
-	var presenter = scrapePage([path]);
+	
+	//doesn't work yet
+	var presenter = document.getElementsByName("7.16.0.0.0.0.2.7.0.0.1.4.3.1.5.5.0.1.3.1.5.0.0.1.16.21.21.1.0.3.3.0.1.0.5.0.0.0.5.1.1.1.0.1.3.0.1.0.1.3.1").value
 	
 	//clean up spaces
 	eventName = eventName.replace(" ","+");
