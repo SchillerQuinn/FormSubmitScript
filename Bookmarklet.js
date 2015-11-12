7dc8396a6fc149faab30ff4ec16bf9bad9e3eebd
SchillerQuinn <SchillerQuinn@gmail.com>
2015-11-12 14:40:15 -0600

    pretified

--- Bookmarklet.js
+++ Bookmarklet.js
@@ -1,6 +1,6 @@
-javascript: (function () { 
-    var jsCode = document.createElement('script'); 
-    jsCode.setAttribute('src', 'https://rawgit.com/SchillerQuinn/FormSubmitScript/master/Scraper.js');                      
-  document.body.appendChild(jsCode); 
-  console.log('Loaded externally...')
+javascript: (function () {
+    var jsCode = document.createElement('script');
+    jsCode.setAttribute('src', 'https://rawgit.com/SchillerQuinn/FormSubmitScript/master/Scraper.js');
+    document.body.appendChild(jsCode);
+    console.log('Loaded scraper.js externally...')
  }());
